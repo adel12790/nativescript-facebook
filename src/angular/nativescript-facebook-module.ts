2b517b1cccc6a23ebc9f610e2525c0101d5ff8f2
Mohamed Adel <mohamed@chiliz.com>
2021-01-19 15:44:25 +0100

    misc for angular build not fixed yet

--- src/angular/nativescript-facebook-module.ts
+++ src/angular/nativescript-facebook-module.ts
@@ -3,12 +3,14 @@ import { registerElement } from "nativescript-angular/element-registry";
 
 import { DIRECTIVES } from "./nativescript-facebook-directives";
 
+import { LoginButton, ShareButton, SendButton } from "../";
+
 @NgModule({
     declarations: [DIRECTIVES],
     exports: [DIRECTIVES],
 })
 export class NativeScriptFacebookModule { }
 
-registerElement("FacebookLoginButton", () => require("../").LoginButton);
-registerElement('FacebookShareButton', () => require('../').ShareButton);
-registerElement('FacebookSendButton', () => require('../').SendButton);
+registerElement("FacebookLoginButton", () => LoginButton);
+registerElement('FacebookShareButton', () => ShareButton);
+registerElement('FacebookSendButton', () => SendButton);
